de2783ad3ec17d864b9c6ae267b0b04b76e5e36b
Benjamin Almack <benjaminalmack@gmail.com>
2025-12-11 15:24:20 -0500

    Quartz sync: Dec 11, 2025, 3:24 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Home",
+    pageTitle: "Campaign Wiki",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
